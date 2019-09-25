8a14eaaa855e3397c35e6ae2b75d159a89071d52
Patrick Westerhoff <PatrickWesterhoff@gmail.com>
2019-09-26 00:41:10 +0200

    Create project

---
+++ README.md
@@ -0,0 +1,3 @@
+# RazorBlazor
+
+This is an example ASP.NET Core 3.0 application that uses both Razor pages and server-side Blazor within the same application. The Blazor component is being embedded directly into Razor pages, without involving client-side routing or other SPA-related setups.
---
+++ Startup.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RazorBlazor
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
